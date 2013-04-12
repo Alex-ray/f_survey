0bde49fd0ced897a2ada912ef2b303d3ed7ea339
Eric Dykstra <eric@ericdykstra.me>
2013-04-12 16:54:22 -0700

    added signup route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,17 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 get '/:survey_id' do
   @survey = Survey.where("id = ?", params[:survey_id])
   erb :index
+end
+
+post '/signup' do
+  puts params
+  @user = User.create params
+end
+
+post 'login' do
+
 end
