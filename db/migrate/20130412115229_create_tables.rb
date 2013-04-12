167ae00f5f32b1ec76a9631726560a52a537920d
AlexRay <alex.james.ray@gmail.com>
2013-04-12 15:36:50 -0700

    created database

---
+++ db/migrate/20130412115229_create_tables.rb
@@ -0,0 +1,32 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :users do |u|
+      u.string :password_hash, :email
+      u.timestamps
+    end
+
+    create_table :surveys do |s|
+      s.string :title
+      s.references :user
+      s.timestamps
+    end
+
+    create_table :questions do |q|
+      q.string :text
+      q.references :survey
+      q.timestamps 
+    end
+
+    create_table :choices do |a|
+      a.string :name
+      a.references :user, :question
+      a.timestamps
+    end
+
+    create_table :images do |i|
+      i.string :url
+      i.references :survey
+      i.timestamps
+    end
+  end
+end
